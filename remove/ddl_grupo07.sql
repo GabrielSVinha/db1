0f613959e7a182694ac28af6b047c4ac3eaa5921
Kaio Oliveira <kaio.moura@live.com>
2017-07-28 13:05:49 -0300

    dropando tabelas existentes ate o momento

--- remove/ddl_grupo07.sql
+++ remove/ddl_grupo07.sql
@@ -1,10 +1,12 @@
-DROP TABLE IF EXISTS dependentes;
+USE [hotel]
 
-DROP TABLE IF EXISTS clientes;
+ALTER TABLE [dbo].[produtos] DROP CONSTRAINT [servicoId]
+ALTER TABLE [dbo].[produtos] DROP CONSTRAINT [quartoProd]
+ALTER TABLE [dbo].[dependentes] DROP CONSTRAINT [dependenteId]
+ALTER TABLE [dbo].[clientes] DROP CONSTRAINT [quartoUser]
 
-DROP TABLE IF EXISTS produtos;
-
-DROP TABLE IF EXISTS servicos;
-
-DROP TABLE IF EXISTS quartos;
-
+DROP TABLE IF EXISTS servicos
+DROP TABLE IF EXISTS quartos
+DROP TABLE IF EXISTS produtos
+DROP TABLE IF EXISTS dependentes
+DROP TABLE IF EXISTS clientes
