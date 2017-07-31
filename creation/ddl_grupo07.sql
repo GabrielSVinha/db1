26a04348d892cb10e8b18c7b88392e54c1823e81
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2017-07-31 10:22:23 -0300

    mudancas em atributo de clientes e no tamanho dos dados

--- creation/ddl_grupo07.sql
+++ creation/ddl_grupo07.sql
@@ -1,28 +1,28 @@
 CREATE TABLE quartos(
   quarto_id NUMERIC NOT NULL,
   numero NUMERIC,
-  tipo VARCHAR(5),
+  tipo VARCHAR(10),
   vista VARCHAR(10),
   diaria NUMERIC,
   PRIMARY KEY(quarto_id)
 );
 
 CREATE TABLE clientes(
-  nome VARCHAR(15) 	NOT NULL,
+  nome VARCHAR(50) 	NOT NULL,
   cpf CHAR(11) 		NOT NULL,
   dataNascimento TIMESTAMP,
   sexo CHAR(1)		,
   endereco VARCHAR(100)	,
   telefone CHAR(11)	,
-  email VARCHAR(15)	,
-  quarto_id NUMERIC NOT NULL,
+  email VARCHAR(30)	,
+  quartoId NUMERIC NOT NULL,
   PRIMARY KEY(cpf),
-  CONSTRAINT quartoUser FOREIGN KEY(quarto_id)
+  CONSTRAINT quartoUser FOREIGN KEY(quartoId)
     REFERENCES quartos(quarto_id)
 );
 
 CREATE TABLE dependentes(
-  nome VARCHAR(15) NOT NULL,
+  nome VARCHAR(50) NOT NULL,
   dependente_cpf CHAR(11) NOT NULL,
   dataNascimento TIMESTAMP,
   depCpf CHAR(11),
@@ -40,7 +40,7 @@ CREATE TABLE servicos(
 
 CREATE TABLE produtos(
   produto_id NUMERIC NOT NULL,
-  nome VARCHAR(20),
+  nome VARCHAR(50),
   tipo VARCHAR(11),
   descricao VARCHAR(140),
   quarto_id NUMERIC NOT NULL,
