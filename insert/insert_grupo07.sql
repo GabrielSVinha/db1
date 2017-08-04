cb16279e484d0405e18b791a08414e44b3543cc8
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2017-08-04 09:47:57 -0300

    seed de cliente pronto

---
+++ insert/insert_grupo07.sql
@@ -0,0 +1,32 @@
+INSERT ALL
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('12345678912', 'KAIO',      '01-20-1995', '1', 'RUA DAS JABUTICABEIRAS',          100,  'MALVINAS',         '58100000', 'kaio@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('34567891234', 'LUCAS',     '02-23-1994', '1', 'RUA DAS UMBURANAS',               523,  'MALVINAS',         '58100000', 'lucas@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('56789123456', 'GABRIEL',   '03-14-1994', '1', 'RUA XIQUE-XIQUE',                 1542, 'ROCHA CAVALCANTE', '58100000', 'gabriel@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('78912345678', 'ITALO',     '04-12-1995', '1', 'RUA RECIFE',                      12,   'LIBERDADE',        '58100000', 'italo@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('91234567891', 'JAAZIEL',   '05-05-1992', '1', 'RUA MACIEL PINHEIRO',             227,  'CENTRO',           '58100000', 'jaaziel@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('23456789123', 'ALEXANDRO', '06-16-1993', '1', 'AV MAL FLORIANO PEIXOTO',         450,  'SAO JOSE',         '58100000', 'alexandro@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('45678912345', 'FILIPE',    '07-15-1995', '1', 'AV ALMIRANTE BARROSO',            221,  'QUARENTA',         '58100000', 'filipe@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('67891234567', 'ZEUS',      '08-30-1990', '1', 'RUA RIO DE JANEIRO',              75,   'LIBERDADE',        '58100000', 'zeus@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('89123456789', 'VANDERLAN', '09-29-1992', '1', 'RUA OLINDINA PEREIRA DOS SANTOS', 97,   'MALVINAS',         '58100000', 'vanderlan@ccc.ufcg.edu.br')
+    
+    INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+        VALUES ('12345678910', 'DIEGO',     '10-03-1992', '1', 'RUA CECILIA DE ARAUJO',           556,  'TRES IRMAS',       '58100000', 'diego@ccc.ufcg.edu.br')
+
+SELECT 1 FROM DUAL;
