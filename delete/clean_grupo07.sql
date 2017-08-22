db289769ad8c0153ddb48fd971b2c37ddc5a543f
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2017-08-22 00:23:15 -0300

    criando e finalizando script de delete

---
+++ delete/clean_grupo07.sql
@@ -0,0 +1,24 @@
+DELETE FROM equipamento;
+DELETE FROM estacionamento;
+DELETE FROM passeio_turistico;
+
+DELETE FROM telefone;
+DELETE FROM avaliacao;
+
+DELETE FROM venda;
+DELETE FROM produto;
+
+DELETE FROM hospedagem;
+DELETE FROM reserva;
+
+DELETE FROM servico;
+DELETE FROM servico_prestado;
+DELETE FROM restaurante;
+DELETE FROM frigobar;
+DELETE FROM lavanderia;
+DELETE FROM bar;
+
+DELETE FROM dependente;
+DELETE FROM funcionario;
+DELETE FROM quarto;
+DELETE FROM cliente;
