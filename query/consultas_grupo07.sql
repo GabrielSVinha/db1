02293168af8ba350f65b508e2003158a3ce0bd72
kaiokassiano <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2017-08-22 23:22:17 -0300

    ajustes de projecao e de visualizacao das consultas

--- query/consultas_grupo07.sql
+++ query/consultas_grupo07.sql
@@ -5,7 +5,7 @@ GROUP BY c.cpf,c.nome
 ORDER BY c.cpf;
 
 -- 02
-SELECT DISTINCT c.cpf, c.nome FROM cliente c
+SELECT c.* FROM cliente c
 LEFT JOIN hospedagem h ON c.cpf = h.cpf_cliente
 LEFT JOIN equipamento e ON e.id_quarto = h.id_quarto
 WHERE e.equipamento = 'ar condicionado'
@@ -14,58 +14,91 @@ ORDER BY c.cpf;
 -- 03
 CREATE VIEW servico_sem_produto AS
     SELECT * FROM
-        (SELECT l.id_lavanderia as id_servico FROM lavanderia l LEFT JOIN produto p ON l.id_lavanderia = p.id_lavanderia WHERE p.id_lavanderia IS NULL)
+        (
+            SELECT s.* FROM servico s
+            INNER JOIN lavanderia l ON l.id_lavanderia = s.id_servico
+            LEFT JOIN produto p ON l.id_lavanderia = p.id_lavanderia
+            WHERE p.id_lavanderia IS NULL
+        )
         UNION
-        (SELECT f.id_frigobar as id_servico FROM frigobar f LEFT JOIN produto p ON f.id_frigobar = p.id_frigobar WHERE p.id_frigobar IS NULL)
+        (
+            SELECT s.* FROM servico s
+            INNER JOIN frigobar f ON f.id_frigobar = s.id_servico
+            LEFT JOIN produto p ON f.id_frigobar = p.id_frigobar
+            WHERE p.id_frigobar IS NULL
+        )
         UNION
-        (SELECT r.id_rest as id_servico FROM restaurante r LEFT JOIN produto p ON r.id_rest = p.id_rest WHERE p.id_rest IS NULL)
+        (
+            SELECT s.* FROM servico s
+            INNER JOIN restaurante r ON r.id_rest = s.id_servico
+            LEFT JOIN produto p ON r.id_rest = p.id_rest
+            WHERE p.id_rest IS NULL
+        )
         UNION
-        (SELECT b.id_bar as id_servico FROM bar b LEFT JOIN produto p ON b.id_bar = p.id_bar WHERE p.id_bar IS NULL)
+        (
+            SELECT s.* FROM servico s
+            INNER JOIN bar b ON b.id_bar = s.id_servico
+            LEFT JOIN produto p ON b.id_bar = p.id_bar
+            WHERE p.id_bar IS NULL
+        )
         UNION
-        (SELECT id_estacionamento as id_servico FROM estacionamento)
+        (
+            SELECT s.* FROM servico s
+            INNER JOIN estacionamento e ON e.id_estacionamento = s.id_servico
+        )
         UNION
-        (SELECT id_passeio as id_servico FROM passeio_turistico);
+        (
+            SELECT s.* FROM servico s
+            INNER JOIN passeio_turistico p ON p.id_passeio = s.id_servico
+        )
+    ;
 
 -- 04
-SELECT * FROM cliente c
+SELECT c.* FROM cliente c
 LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
 LEFT JOIN equipamento e ON r.id_quarto = e.id_quarto
-WHERE e.equipamento = 'televisao';
+WHERE e.equipamento = 'televisao'
+ORDER BY c.cpf;
 
 -- 05
-CREATE VIEW homens_reserva_2016 AS SELECT * FROM cliente c
-LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
-WHERE
-  c.sexo = 'M' AND (EXTRACT(YEAR FROM r.data_inicio) = 2016 OR EXTRACT(YEAR FROM r.data_fim) = 2016);
+CREATE VIEW homens_reserva_2016 AS
+    SELECT c.* FROM cliente c
+    LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
+    WHERE c.sexo = 'M' AND
+        (EXTRACT(YEAR FROM r.data_inicio) = 2016
+        OR
+        EXTRACT(YEAR FROM r.data_fim) = 2016);
 
 -- 06
 SELECT
-  SUM(v.quantidade * p.valor) AS lucro_bar
+  SUM(v.quantidade * p.valor) AS soma_vendas_bar
 FROM venda v
 LEFT JOIN produto p ON v.id_produto = p.id_produto
 WHERE p.id_bar IS NOT NULL;
 
 -- 07
-SELECT * FROM cliente c
+SELECT COUNT(*) as clientes_homens_que_avaliaram FROM cliente c
 INNER JOIN avaliacao a ON a.cpf_cliente = c.cpf
 WHERE c.sexo = 'M';
 
 -- 08
-SELECT * FROM cliente c
+SELECT c.* FROM cliente c
 LEFT JOIN hospedagem h ON c.cpf = h.cpf_cliente
 LEFT JOIN quarto q ON q.id_quarto = h.id_quarto
 WHERE c.sexo = 'M' AND q.numero = 101;
 
 -- 09
-SELECT MAX(data_fim - data_inicio) as tempo_de_reserva_em_dias FROM reserva;
+SELECT MAX(data_fim - data_inicio) as maior_reserva_em_dias FROM reserva;
 
 -- 10
-SELECT * FROM cliente c
+SELECT c.* FROM cliente c
 LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
 WHERE
   data_nascimento IN (SELECT MAX(data_nascimento) FROM cliente)
   AND
-  (EXTRACT(YEAR FROM r.data_inicio) = EXTRACT(YEAR FROM SYSDATE) - 1 OR EXTRACT(YEAR FROM r.data_fim) = EXTRACT(YEAR FROM SYSDATE) - 1);
+    (EXTRACT(YEAR FROM r.data_inicio) = EXTRACT(YEAR FROM SYSDATE) - 1
+    OR
+    EXTRACT(YEAR FROM r.data_fim) = EXTRACT(YEAR FROM SYSDATE) - 1);
 
 -- 11
 SELECT q.diaria FROM quarto q
@@ -74,12 +107,12 @@ LEFT JOIN produto p ON p.id_produto = v.id_produto
 WHERE p.id_bar IS NOT NULL;
 
 -- 12
-SELECT * FROM cliente c
+SELECT c.* FROM cliente c
 INNER JOIN dependente d ON c.cpf = d.titular
 WHERE c.rua LIKE '%Aprigio%';
 
 -- 13
-SELECT * FROM funcionario f
+SELECT f.* FROM funcionario f
 LEFT JOIN servico_prestado sp ON sp.cpf_func = f.cpf
 INNER JOIN bar b ON sp.id_servico = b.id_bar;
 
