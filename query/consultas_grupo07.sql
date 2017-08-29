1db6dee4ef8e40d301a60da26a4db3d74383cd05
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-08-29 13:32:24 -0300

    Corrige espacos para entrega

--- query/consultas_grupo07.sql
+++ query/consultas_grupo07.sql
@@ -1,15 +1,15 @@
 -- 01
 SELECT c.cpf, c.nome, COUNT(d.cpf) AS qtd_dep FROM cliente c
-LEFT JOIN dependente d ON c.cpf = d.titular
-GROUP BY c.cpf,c.nome
-ORDER BY c.cpf;
+  LEFT JOIN dependente d ON c.cpf = d.titular
+  GROUP BY c.cpf,c.nome
+  ORDER BY c.cpf;
 
 -- 02
 SELECT c.* FROM cliente c
-LEFT JOIN hospedagem h ON c.cpf = h.cpf_cliente
-LEFT JOIN equipamento e ON e.id_quarto = h.id_quarto
-WHERE e.equipamento = 'ar condicionado'
-ORDER BY c.cpf;
+  LEFT JOIN hospedagem h ON c.cpf = h.cpf_cliente
+  LEFT JOIN equipamento e ON e.id_quarto = h.id_quarto
+  WHERE e.equipamento = 'ar condicionado'
+  ORDER BY c.cpf;
 
 -- 03
 CREATE VIEW servico_sem_produto AS
@@ -55,10 +55,10 @@ CREATE VIEW servico_sem_produto AS
 
 -- 04
 SELECT c.* FROM cliente c
-LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
-LEFT JOIN equipamento e ON r.id_quarto = e.id_quarto
-WHERE e.equipamento = 'televisao'
-ORDER BY c.cpf;
+  LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
+  LEFT JOIN equipamento e ON r.id_quarto = e.id_quarto
+  WHERE e.equipamento = 'televisao'
+  ORDER BY c.cpf;
 
 -- 05
 CREATE VIEW homens_reserva_2016 AS
@@ -72,28 +72,28 @@ CREATE VIEW homens_reserva_2016 AS
 -- 06
 SELECT
   SUM(v.quantidade * p.valor) AS soma_vendas_bar
-FROM venda v
-LEFT JOIN produto p ON v.id_produto = p.id_produto
-WHERE p.id_bar IS NOT NULL;
+  FROM venda v
+  LEFT JOIN produto p ON v.id_produto = p.id_produto
+  WHERE p.id_bar IS NOT NULL;
 
 -- 07
 SELECT COUNT(*) as clientes_homens_que_avaliaram FROM cliente c
-INNER JOIN avaliacao a ON a.cpf_cliente = c.cpf
-WHERE c.sexo = 'M';
+  INNER JOIN avaliacao a ON a.cpf_cliente = c.cpf
+  WHERE c.sexo = 'M';
 
 -- 08
 SELECT c.* FROM cliente c
-LEFT JOIN hospedagem h ON c.cpf = h.cpf_cliente
-LEFT JOIN quarto q ON q.id_quarto = h.id_quarto
-WHERE c.sexo = 'M' AND q.numero = 101;
+  LEFT JOIN hospedagem h ON c.cpf = h.cpf_cliente
+  LEFT JOIN quarto q ON q.id_quarto = h.id_quarto
+  WHERE c.sexo = 'M' AND q.numero = 101;
 
 -- 09
 SELECT MAX(data_fim - data_inicio) as maior_reserva_em_dias FROM reserva;
 
 -- 10
 SELECT c.* FROM cliente c
-LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
-WHERE
+  LEFT JOIN reserva r ON c.cpf = r.cpf_cliente
+  WHERE
   data_nascimento IN (SELECT MAX(data_nascimento) FROM cliente)
   AND
     (EXTRACT(YEAR FROM r.data_inicio) = EXTRACT(YEAR FROM SYSDATE) - 1
@@ -102,22 +102,22 @@ WHERE
 
 -- 11
 SELECT q.diaria FROM quarto q
-LEFT JOIN venda v ON v.id_quarto = q.id_quarto
-LEFT JOIN produto p ON p.id_produto = v.id_produto
-WHERE p.id_bar IS NOT NULL;
+  LEFT JOIN venda v ON v.id_quarto = q.id_quarto
+  LEFT JOIN produto p ON p.id_produto = v.id_produto
+  WHERE p.id_bar IS NOT NULL;
 
 -- 12
 SELECT c.* FROM cliente c
-INNER JOIN dependente d ON c.cpf = d.titular
-WHERE c.rua LIKE '%Aprigio%';
+  INNER JOIN dependente d ON c.cpf = d.titular
+  WHERE c.rua LIKE '%Aprigio%';
 
 -- 13
 SELECT f.* FROM funcionario f
-LEFT JOIN servico_prestado sp ON sp.cpf_func = f.cpf
-INNER JOIN bar b ON sp.id_servico = b.id_bar;
+  LEFT JOIN servico_prestado sp ON sp.cpf_func = f.cpf
+  INNER JOIN bar b ON sp.id_servico = b.id_bar;
 
 -- 14
 SELECT s.id_servico, AVG(a.nota) as media_nota FROM servico s
-INNER JOIN avaliacao a ON a.id_servico = s.id_servico
-GROUP BY s.id_servico
-ORDER BY s.id_servico
+  INNER JOIN avaliacao a ON a.id_servico = s.id_servico
+  GROUP BY s.id_servico
+  ORDER BY s.id_servico
